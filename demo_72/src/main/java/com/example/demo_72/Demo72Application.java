35ca34a6fbbcb7dc3009103d2ea31786553c84a2
sunyizegithub <1335445690@qq.com>
2020-07-15 13:44:31 +0800

    提交

---
+++ demo_72/src/main/java/com/example/demo_72/Demo72Application.java
@@ -0,0 +1,11 @@
+package com.example.demo_72;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo72Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo72Application.class, args);
+    }
+}
